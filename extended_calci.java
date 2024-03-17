cebdac395c1d692443eace013339070d5d311b6b
01fe22bca225 <01fe22bca225@kletech.ac.in>
2024-03-17 22:02:44 +0530

    merging 2 programs

---
+++ extended_calci.java
@@ -0,0 +1,27 @@
+public class extended_calci {
+   
+    public int areaoftri(){
+        int base= 10;
+        int height = 5;
+        int area ;
+        area = (1/2)*base*height ;
+        return area ;
+
+    }
+
+    public int areaofsq(){
+        int side=5;
+        int area_of_sq= side*side;
+        return area_of_sq;
+    }
+
+    public int modulus(){
+        int a= 10;
+        int b= 5;
+        int mod= a%b;
+        return mod;
+    }
+}
+
+
+
